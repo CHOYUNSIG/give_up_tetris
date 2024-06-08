3c3fd293166390dcd05be7e9acbd8065efb74beb
조윤식 <cho.yunsig56@gmail.com>
2024-06-09 05:08:32 +0900

    버그 수정

--- res/font/font.py
+++ res/font/font.py
@@ -1,5 +1,6 @@
 import pygame
+import os
+
 pygame.init()
 
-
-fonts = [pygame.font.Font("../res/font/D2Coding-Ver1.3.2-20180524.ttf", 6 * i) for i in range(10)]
+fonts = [pygame.font.Font(os.path.dirname(os.path.abspath(__file__)) + "/D2Coding-Ver1.3.2-20180524.ttf", 6 * i) for i in range(10)]
